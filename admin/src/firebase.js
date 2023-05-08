14c6e8fe4134f3f17fb7b73be7a2d108c25b70de
sairevanth0007 <sairevanth0007@gmail.com>
2023-05-08 17:09:45 +0530

    Final Upload

---
+++ admin/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_z0zBq8BV4tMax60OB-iszwaJcXYbysU",
+  authDomain: "b2becom-de3e1.firebaseapp.com",
+  projectId: "b2becom-de3e1",
+  storageBucket: "b2becom-de3e1.appspot.com",
+  messagingSenderId: "372814318871",
+  appId: "1:372814318871:web:1ef7eb3edb3d05a6ffbb52"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
